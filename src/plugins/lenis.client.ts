2a0f8f62946d5fb32f05788a0eb03126e56a0cfa
gerayzade <hgarayzada@gmail.com>
2026-05-10 07:12:04 +0400

    updated scroller

--- src/plugins/lenis.client.ts
+++ src/plugins/lenis.client.ts
@@ -5,7 +5,25 @@ import Lenis from 'lenis'
 import 'lenis/dist/lenis.css'
 
 export default defineNuxtPlugin(() => {
-  const lenis = new Lenis({ autoRaf: false })
+  const wrapper = document.getElementById('__nuxt')
+
+  if (!wrapper) {
+    return
+  }
+
+  ScrollTrigger.defaults({ scroller: wrapper })
+
+  const content = wrapper.firstElementChild
+
+  if (!content) {
+    return
+  }
+
+  const lenis = new Lenis({
+    autoRaf: false,
+    wrapper,
+    content,
+  })
 
   lenis.on('scroll', ScrollTrigger.update)
 
@@ -13,6 +31,11 @@ export default defineNuxtPlugin(() => {
     lenis.raf(time * 1000)
   })
   gsap.ticker.lagSmoothing(0)
+
+  ScrollTrigger.addEventListener('refresh', () => {
+    lenis.resize()
+  })
+  ScrollTrigger.refresh()
 
   return {
     provide: {
