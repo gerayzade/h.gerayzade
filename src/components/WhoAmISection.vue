42496a4b92cc2e52d712fee22f0a935c7b51cc5c
gerayzade <hgarayzada@gmail.com>
2026-05-18 00:01:59 +0400

    updated typography

--- src/components/WhoAmISection.vue
+++ src/components/WhoAmISection.vue
@@ -10,7 +10,7 @@
     >
     <div class="who-am-i-text absolute inset-0 z-10 flex items-end justify-center p-16 max-xs:pl-4">
       <p class="max-w-screen-sm text-balance font-medium leading-normal tracking-tighter xs:text-2xl">
-        <span class="relative bottom-0.5 ml-px mr-2 bg-indigo-600 p-1 text-sm font-black text-white">
+        <span class="relative ml-px mr-1 bg-indigo-600 p-0.5 text-xs font-black text-white xs:bottom-0.5 xs:mr-2 xs:p-1 xs:text-sm">
           Who am I.
         </span>
         <mark
@@ -19,7 +19,7 @@
         >
           Based in Baku, focused on building scalable web applications with
           JavaScript frameworks and libraries. Currently I work primarily with Vue.js,
-          Nuxt v4 and TypeScript to create performant and user-friendly products.
+          Nuxt v4 and TypeScript to create performant and accessible products.
           With a background in Computer Science studies at ADA University and years
           of professionnal carrer, I enjoy turning complex ideas into clean, intuitive
           interfaces while continuously learning and collaborating with strong teams.
